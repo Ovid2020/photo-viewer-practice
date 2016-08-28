1775471b6b4693f5ed5f249f4fcdae154ee7dfb7
Vincent Barilla <vincentbarilla@Vincents-MacBook-Pro.local>
2016-08-28 16:59:13 -0700

    Implement basic server for static files

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static(__dirname + '/../client'));
+app.use('/files', express.static(__dirname + '/../node_modules'));
+
+app.get('/', function(req, res){
+	res.sendfile('index.html');
+});
+
+console.log("listening on 3000");
+app.listen(3000);
